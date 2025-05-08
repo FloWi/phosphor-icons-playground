bd3d706d5e89d1c8baf25528e3716bf46a7089e8
Florian Witteler <github@flwi.de>
2025-05-08 17:38:01 +0200

    initial commit testing phosphor icons

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use leptos::leptos_dom::log;
+use leptos::prelude::*;
+use phosphor_leptos::{Icon, IconWeight, CUBE, HEART, HORSE};
+
+
+fn main() {
+    log!("Hello, Horse Icon");
+    log!("{}", HORSE.get(IconWeight::Regular));
+
+    leptos::mount::mount_to_body(|| view! {
+        <div>
+            <p>"Hello, world!"</p>
+            <MyComponent />
+        </div>
+    })
+}
+
+#[component]
+fn MyComponent() -> impl IntoView {
+
+    let icons = vec![
+        ("HORSE", HORSE),
+        ("HEART", HEART),
+        ("CUBE", CUBE)
+    ];
+
+    view! {
+        {icons
+            .into_iter()
+            .map(|(label, icon)| {
+                view! {
+                    <div>
+                        <p>{label}</p>
+                        <p>{icon.get(IconWeight::Regular)}</p>
+                        <Icon icon=icon color="#AE2983" weight=IconWeight::Fill size="32px" />
+                    </div>
+                }
+            })
+            .collect_view()
+        }
+    }
+}
